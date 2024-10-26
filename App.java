858876a9a57a6c00a959ea32bc675d8ca35f93b7
tester <tester@test.com>
2024-10-26 15:24:24 +0200

    uzycie while aby program sie nie zamykał po podaniu blednego inputu

--- App.java
+++ App.java
@@ -2,27 +2,30 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Podaj liczbe");
 
-        try {
-            int number = scanner.nextInt();
-            for (int j = 0; j < number; j++) {
-                System.out.println();
-                for (int i = 1; i <= number; i++) {
-                    if (i == number) {
-                        System.out.print(i);
-                    } else {
-                        System.out.print(i + ",");
+        boolean stopRunning = false;
+
+        while (!stopRunning) {
+            try {
+                Scanner scanner = new Scanner(System.in);
+                System.out.println("Podaj liczbe");
+                int number = scanner.nextInt();
+                for (int j = 0; j < number; j++) {
+                    System.out.println();
+                    for (int i = 1; i <= number; i++) {
+                        if (i == number) {
+                            System.out.print(i);
+                        } else {
+                            System.out.print(i + ",");
+                        }
+                        stopRunning = true;
                     }
                 }
+
+            } catch (Exception e) {
+                System.out.println("Podales litere zamiast cyfry");
+
             }
-        } catch (Exception e) {
-            System.out.println("Podales litere zamiast cyfry");
         }
-
-
-
-
     }
 }
