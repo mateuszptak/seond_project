0f41254f47a5cd16e123121aa08bc2da0485e331
Mateusz <mateusz.ptak2612@gmail.com>
2024-11-09 17:38:39 +0100

    Create new Car class and make constructor

--- Test.java
+++ Test.java
@@ -5,14 +5,45 @@ public class Test {
 //
 //        Utils utils = new Utils();
 //        utils.inputINtObject("Siema, ile dasz mi szlugów?");
-        Student student = new Student("Mateusz", "Ptak", 11);
-        System.out.println(student);
+//        Student student = new Student("Mateusz", "Ptak", 11);
+//        System.out.println(student);
+//
+//        System.out.println(student.getAge());
+//
+//        System.out.println(student.getFullName());
 
-        System.out.println(student.getAge());
 
-        System.out.println(student.getFullName());
+        Car myCar = new Car("White", "BMW");
+
+        System.out.println("I have " + myCar.getMake() + " and it's color is " + myCar.getColor());
+
+
     }
 }
+
+class Car {
+   private String color;
+    private String make;
+
+    public Car(String color, String make) {
+        this.make = make;
+        this.color = color;
+    }
+
+    public String getColor(){
+        return color;
+    }
+
+    public String getMake(){
+        return make;
+    }
+}
+
+
+
+
+
+
 
 class Student {
    private String name;
