ade10fe37d66f73eaca99891b1f674cd7ddce5f0
Mateusz <mateusz.ptak2612@gmail.com>
2024-11-09 17:23:27 +0100

    how to Create constructor

--- Test.java
+++ Test.java
@@ -1,9 +1,23 @@
 public class Test {
     public static void main(String[] args) {
-        int firstNumber = Utils.input("Podaj liczbe:");
-        System.out.println(firstNumber);
+//        int firstNumber = Utils.input("Podaj liczbe:");
+//        System.out.println(firstNumber);
+//
+//        Utils utils = new Utils();
+//        utils.inputINtObject("Siema, ile dasz mi szlugów?");
+        Student student = new Student("Mateusz", "Ptak", 11);
+        System.out.println(student);
+    }
+}
 
-        Utils utils = new Utils();
-        utils.inputINtObject("Siema, ile dasz mi szlugów?");
+class Student {
+   private String name;
+   private String surname;
+   private int age;
+
+    public Student(String name, String surname, int age) {
+        this.name = name;
+        this.surname = surname;
+        this.age = age;
     }
 }
